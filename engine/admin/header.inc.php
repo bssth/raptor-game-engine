3a7b0a964152179b9f794c0e36a69c611b9c7ad8
Boris Stelmah <mikael.chip@gmail.com>
2015-06-26 22:10:40 +0300

    Update header.inc.php

--- engine/admin/header.inc.php
+++ engine/admin/header.inc.php
@@ -29,7 +29,7 @@
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-                    <a class="navbar-brand" href="index">Админ-панель <?= $GLOBALS['name']; ?></a>
+                    <a class="navbar-brand" href="index">Админ-панель <?= @$GLOBALS['name']; ?></a>
                 </div>
 
                 <ul class="nav navbar-right top-nav">
