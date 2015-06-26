3d9029f234dd4de8b85422a6c7938f41af7a2397
Boris Stelmah <mikael.chip@gmail.com>
2015-06-26 22:27:41 +0300

    admin hotfix

--- engine/drivers/admin.php
+++ engine/drivers/admin.php
@@ -6,11 +6,10 @@ class adminDriver {
     {
 		$func = strtolower(str_replace("action", "", $func));
 		if(!file_exists(CACHE_ROOT . SEPARATOR . "installed.cache") and $func != 'install') { header("Location: /admin/install"); die(); }
-		if(!isset($_SESSION['cid'])) { die("403 Forbidden"); }
-        $char = new Char($_SESSION['cid']);
-        if ($char->admin<1 and !in_array($func, $char->perms) and file_exists(CACHE_ROOT . SEPARATOR . "installed.cache")) {
+        if (char()->admin<1 and !in_array($func, $char->perms) and file_exists(CACHE_ROOT . SEPARATOR . "installed.cache")) {
             die("403 Forbidden");
         }
+        if(!isset($_SESSION['cid']) and file_exists(CACHE_ROOT . SEPARATOR . "installed.cache")) { die("403 Forbidden"); }
         include_once(ADMIN_ROOT . SEPARATOR . "header.inc.php");
         if (strstr($func, 'ext_')) {
             include_once(MODS_ROOT . SEPARATOR . trim($func, 'ext_') . SEPARATOR . trim($func, 'ext_') . ".admin.php");
