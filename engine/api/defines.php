b0a3ad7f6b48e9968cdb4a45f8ef53cd4511bc94
Boris Stelmah <mikael.chip@gmail.com>
2015-06-28 19:13:23 +0300

    dev-4

--- engine/api/defines.php
+++ engine/api/defines.php
@@ -4,7 +4,7 @@ define("STRING_SEPARATOR", " ");
 define("API_DIRECTORY", __DIR__);
 define("R_DEFINES", __FILE__);
 
-define("ENGINE_VERSION", "dev-2");
+define("ENGINE_VERSION", "dev4");
 define("RAPTOR_URL", "http://raptor.blockstudio.net");
 
 define("ENGINE_NAME", "Raptor Game Engine");
