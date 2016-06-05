7176cce362106388b4735c590e48c49a393fd559
Boris Stelmah <mikael.chip@gmail.com>
2016-06-05 23:16:03 +0300

    first functional admin

--- engine/modules/admin/Controller/Admin.php
+++ engine/modules/admin/Controller/Admin.php
@@ -12,13 +12,34 @@
 			$char = new \Auth\Char($_SESSION['cid']);
 			if($char->checkPermission('admin.' . $act) or $char->checkPermission('admin.all'))
 			{
-				$action = (new \Raptor\Templater('admin_' . $act))->render();
-				return (new \Raptor\Templater('admin'))->set('content', $action)->render();
+				try {
+					$action = (new \Raptor\Templater('admin_' . $act))->render();
+				}
+				catch(\Raptor\Exception $e) {
+					return '<h1>404 Not Found</h1>';
+				}
+				if(isset($_GET['embed']))
+				{
+					return $action;
+				}
+				return (new \Raptor\Templater('admin'))->set('char', $char)->set('menu', $this->getMenu())->set('content', $action)->render();
 			}
 			else
 			{
 				return '<h1>403 Forbidden</h1>';
 			}
+		}
+		
+		protected function getMenu()
+		{
+			$test = \Database\Cache::get('admin_menu');
+			if(is_array($test)) {
+				return $test;
+			}
+			
+			$data = \Database\Current::getAll('amenu', array());
+			#\Database\Cache::set('admin_menu', $data, null, 3600); // todo
+			return $data;
 		}
 		
 	}
